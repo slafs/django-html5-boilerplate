e98d678b931eba453e7222092a995028f8e49ffc
Sławek Ehlert <slafs@op.pl>
2011-03-28 00:39:31 +0200

    hide testapp

--- projectname/urls.py
+++ projectname/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^testapp/', include('testapp.urls')),
+    #(r'^testapp/', include('testapp.urls')),
 
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
